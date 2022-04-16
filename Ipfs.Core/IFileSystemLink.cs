27f04456dca6f81bc28bbf889560d6110e39d2f4
William Forney <bill@improvgroup.com>
2022-04-15 17:16:12 -0700

    Remove ipfs.core from this repo - go do pull request to other one if you want to update it

--- Ipfs.Core/IFileSystemLink.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ipfs
-{
-    /// <summary>
-    ///    A link to another file system node in IPFS.
-    /// </summary>
-    public interface IFileSystemLink : IMerkleLink
-    {
-    }
-}
